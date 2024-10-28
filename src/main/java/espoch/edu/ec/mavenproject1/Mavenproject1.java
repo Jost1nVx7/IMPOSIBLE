43e3f49ce95ec697c7d69e65065ddf722f15b22a
Jost1nVx7 <jostinjd2002@gmail.com>
2024-10-28 15:27:53 -0500

    esperanza

---
+++ src/main/java/espoch/edu/ec/mavenproject1/Mavenproject1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.mavenproject1;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class Mavenproject1 {
+
+    public class TablaDelDos {
+    public static void main(String[] args) {
+        int i = 1; // Inicializamos el contador
+
+        // Usamos un bucle while para mostrar la tabla del 2
+        while (i <= 10) {
+            int resultado = 2 * i; // Calculamos el resultado
+            System.out.println("2 x " + i + " = " + resultado); // Mostramos el resultado
+            i++; // Incrementamos el contador
+        }
+    }
+}
+}
